d8940e250a6160352aaf981b99eadfcfadf1fc0e
mrsmamasia <karymzhan39@gmail.com>
2021-02-17 12:04:45 +0600

    16.02

---
+++ src/store/feathers/services/orders.service.js
@@ -0,0 +1,50 @@
+import feathersClient, {
+  makeServicePlugin
+} from "@/store/feathers/feathers.client.js";
+
+import Model from "../models/orders.model.js";
+
+const servicePath = "feathers";
+// for more information see https://vuex.feathersjs.com/service-plugin.html#configuration
+const servicePlugin = makeServicePlugin({
+  Model,
+  servicePath,
+  namespace: "order",
+  service: feathersClient.service(servicePath),
+  state: {},
+  getters: {},
+  mutations: {},
+  actions: {}
+});
+
+feathersClient.service(servicePath).hooks({
+  before: {
+    all: [],
+    find: [],
+    get: [],
+    create: [],
+    update: [],
+    patch: [],
+    remove: []
+  },
+  after: {
+    all: [],
+    find: [],
+    get: [],
+    create: [],
+    update: [],
+    patch: [],
+    remove: []
+  },
+  error: {
+    all: [],
+    find: [],
+    get: [],
+    create: [],
+    update: [],
+    patch: [],
+    remove: []
+  }
+});
+
+export default servicePlugin;
