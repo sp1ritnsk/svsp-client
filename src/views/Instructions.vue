453f44bcf08d7d602539bca6267ba59b1acd6e79
mrsmamasia <karymzhan39@gmail.com>
2021-03-05 16:21:30 +0600

    home page front changes

--- src/views/Instructions.vue
+++ src/views/Instructions.vue
@@ -1,3 +1,56 @@
 <template>
-  <div>Instructions page will be here</div>
+  <v-app>
+    <section>
+      <h1 style="font-size: 3em;" class="text-center mb-1">
+        Инструкции по настройки RTK режимов в ГНСС приемниках
+      </h1>
+      <v-layout row wrap justify-center class="my-5">
+        <v-flex xs12 sm2>
+          <v-card max-width="300">
+            <img src="http://svsn.kz/static/images/Leica_gs08.jpg" />
+            <a href="src/assets/instructions/setup_leica.pdf"
+              >Инструкция по настройке оборудования Leica CS10 и CS15 | PDF</a
+            >
+          </v-card>
+        </v-flex>
+        <v-flex xs12 sm2>
+          <v-card max-width="300">
+            <img src="http://svsn.kz/static/images/Trimble_TSC3_logo.jpg" />
+            <a href="src/assets/instructions/setup_trimble.pdf"
+              >Инструкция по настройке оборудования Trimble TSC3 | PDF</a
+            >
+          </v-card>
+        </v-flex>
+        <v-flex xs12 sm2>
+          <v-card max-width="300">
+            <img src="http://svsn.kz/static/images/stonex4.jpg" />
+            <a href="src/assets/instructions/setup_stonex.pdf"
+              >Инструкция по настройке оборудования Stonex | PDF</a
+            >
+          </v-card>
+        </v-flex>
+        <v-flex xs12 sm2>
+          <v-card max-width="300">
+            <img src="http://svsn.kz/static/images/CHCNav.jpg" />
+            <a href="src/assets/instructions/setup_CHСnav.pdf"
+              >Инструкция по настройке оборудования CHСnav | PDF</a
+            >
+          </v-card>
+        </v-flex>
+        <h1 style="font-size: 3em;" class="text-center mb-1">
+          Инструкции по установке параметров преобразования между различными СК
+        </h1>
+        <v-flex xs12 sm2>
+          <v-card max-width="300">
+            <img src="http://svsn.kz/static/images/Leica_gs08.jpg" />
+            <a href="src/assets/instructions/setup_trimble_cs.pdf"
+              >Инструкция по установке СК на оборудования Trimble TSC3 | PDF</a
+            >
+          </v-card>
+        </v-flex>
+      </v-layout>
+    </section>
+  </v-app>
 </template>
+
+<style lang="scss"></style>
